3cc6b39778e5bc58d572d5895b5dd0c1eaa18a65
Dominic Ridley <domm.rid@gmail.com>
2023-11-15 02:04:32 +0000

    Fix eol sequence

--- config/config.php
+++ config/config.php
@@ -2,9 +2,16 @@
 
 declare(strict_types=1);
 
+use Dridley309\CodeGenerator\Rules\PalindromeRule;
 use Dridley309\CodeGenerator\Strategies\NumericSixStrategy;
 
 return [
     'codes_table' => 'codes',
     'default_strategy' => NumericSixStrategy::class,
+    'strategy_definitions' => [
+        NumericSixStrategy::class => [
+            PalindromeRule::class,
+        ]
+    ],
+    'max_retries' => 10,
 ];
